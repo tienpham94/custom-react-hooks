9eba81fe652cd4094e16e2fe03f0bd53e92b4249
Tien Pham <tiendev94@gmail.com>
2020-04-10 21:10:08 +0300

    Return values

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useToggle } from "../hooks/useToggle";
 
 const Toggle = () => {
-  const [isToggled, setToggle, toggle] = useToggle(false);
+  const { isToggled, toggle } = useToggle(false);
 
   return (
     <div>
--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -3,5 +3,5 @@ import { useState } from "react";
 export const useToggle = initial => {
   const [isToggled, setToggle] = useState(initial);
   const toggle = () => setToggle(s => !s);
-  return [isToggled, setToggle, toggle];
+  return {isToggled, setToggle, toggle};
 };
