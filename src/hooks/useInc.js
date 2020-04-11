a7b97708a010f8bb8afe220f83e8585660d803b5
Tien Pham <tiendev94@gmail.com>
2020-04-11 15:39:15 +0300

    intial

--- src/hooks/useInc.js
+++ src/hooks/useInc.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export const useInc = (initial = 0) => {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(initial);
 
   const inc = () => {
     setValue(s => s + 1);
