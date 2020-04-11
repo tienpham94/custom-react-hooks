b4e89ab6665ed0ffbfb04f9fbc98ea1c18161a17
Tien Pham <tiendev94@gmail.com>
2020-04-11 17:27:47 +0300

    useContext

--- src/App.js
+++ src/App.js
@@ -10,41 +10,46 @@ import Toggle from "./components/Toggle";
 import Mount from "./components/Mount";
 import Inc from "./components/inc";
 import Hover from "./components/Hover";
+import PageWrapper from "./components/PageWrapper";
+import Nav from "./components/Nav";
 
 function App() {
   return (
-    <div>
-      <Header>
-        <Menu />
-        <h1>Header</h1>
-      </Header>
-      <Container>
-        <h2>Super Cool</h2>
-        <Toggle />
-        <Inc />
-        <Mount />
-        <Hover />
+    <PageWrapper>
+      <div>
+        <Header>
+          <Menu />
+          <h1>Header</h1>
+        </Header>
+        <Nav />
+        <Container>
+          <h2>Super Cool</h2>
+          <Toggle />
+          <Inc />
+          <Mount />
+          <Hover />
 
-        <CardGrid>
-          <Card style={{ background: "var(--purp)" }}>
-            <h3>Some card</h3>
-            <img src={purp} />
-          </Card>
-          <Card style={{ background: "var(--blue)" }}>
-            <h3>Some card</h3>
-            <img src={blue} />
-          </Card>
-          <Card style={{ background: "var(--black)" }}>
-            <h3>Some card</h3>
-            <img src={black} />
-          </Card>
-          <Card style={{ background: "var(--green)" }}>
-            <h3>Some card</h3>
-            <img src={green} />
-          </Card>
-        </CardGrid>
-      </Container>
-    </div>
+          <CardGrid>
+            <Card style={{ background: "var(--purp)" }}>
+              <h3>Some card</h3>
+              <img src={purp} />
+            </Card>
+            <Card style={{ background: "var(--blue)" }}>
+              <h3>Some card</h3>
+              <img src={blue} />
+            </Card>
+            <Card style={{ background: "var(--black)" }}>
+              <h3>Some card</h3>
+              <img src={black} />
+            </Card>
+            <Card style={{ background: "var(--green)" }}>
+              <h3>Some card</h3>
+              <img src={green} />
+            </Card>
+          </CardGrid>
+        </Container>
+      </div>
+    </PageWrapper>
   );
 }
 
---
+++ src/components/PageWrapper.js
@@ -0,0 +1,18 @@
+import React, { createContext, useState } from "react";
+import { useToggle } from "../hooks";
+
+export const AppContext = createContext({
+  isMenuOpen: false
+});
+
+const PageWrapper = ({ children }) => {
+  const { isToggled, toggle } = useToggle(false);
+
+  return (
+    <AppContext.Provider value={{ isMenuOpen: isToggled, toggleMenu: toggle }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export default PageWrapper;
